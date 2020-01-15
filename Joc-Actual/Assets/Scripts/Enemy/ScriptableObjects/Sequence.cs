0c094cd32bc1a17627e81de3c984d185d7b16800
OneBogdan01 <31468440+OneBogdan01@users.noreply.github.com>
2020-01-15 10:38:52 +0200

    have the wave manager

---
+++ Joc-Actual/Assets/Scripts/Enemy/ScriptableObjects/Sequence.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "new sequence", menuName = "Create/ new sequence", order = 3)]
+public class Sequence : ScriptableObject
+{
+    public Enemies[] enemies;
+}
+[System.Serializable]
+public struct Enemies
+{
+    public GameObject Enemy;
+    public float DelayForSpawn;
+    public Enemies(GameObject enemy, float delayForSpawn)
+    {
+        this.Enemy = enemy;
+        this.DelayForSpawn = delayForSpawn;
+    }
+}
---
+++ Joc-Actual/Assets/Scripts/WaveManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveManager : MonoBehaviour
+{
+    public Sequence[] sequences;
+    private void Start()
+    {
+        StartCoroutine(SpawnEnemies());
+    }
+    IEnumerator SpawnEnemies()
+    {
+
+        for (int i = 0; i < sequences.Length; i++)
+        {
+
+            for (int j = 0; j < sequences[i].enemies.Length; j++)
+            {
+                Enemies enemy = sequences[i].enemies[j];//have a single variable for short writing
+                Spawn(enemy.Enemy, transform.position);
+                yield return new WaitForSeconds(enemy.DelayForSpawn);
+            }
+        }
+    }
+    void Spawn(GameObject enemy, Vector3 pos)
+    {
+        Instantiate(enemy, pos, Quaternion.identity, transform);
+    }
+}
